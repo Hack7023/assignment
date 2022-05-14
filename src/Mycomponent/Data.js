a31c2d6d5a71f7f6b8e9e216399d1383ad547946
Vishal Kumar <83307176+Hack7023@users.noreply.github.com>
2022-05-14 17:36:54 +0530

    Update Data.js

--- src/Mycomponent/Data.js
+++ src/Mycomponent/Data.js
@@ -1,14 +1,27 @@
 export const imgaeData = {
-"Box1": {add:"./P2.glb",
+"P1.glb": {add:"./P2.glb",
     "gemo":["mesh_95_0","mesh_95_5","mesh_95_2","mesh_95_1","mesh_95_3","mesh_95_4"],
     "material": ['Material.005','Material.006','Material.004','Material.013','Material.014','Material.003']},
 
 
 
 
-"Box2":{add:"./P1.glb",
+"P2.glb":{add:"./P1.glb",
    "gemo":["mesh_92_0","mesh_92_2","mesh_92_4","mesh_92_5","mesh_92_1","mesh_92_3"],
     "material": ['Material.011','Material.009','Material.007','Material.010','Material.008','Material.012']},
+
+    
+"P3.glb":{add:"./P3.glb",
+   "gemo":["mesh_98_0","mesh_98_4","mesh_98_5","mesh_98_2","mesh_98_3","mesh_98_1"],
+    "material": ['Material.044','Material.042','Material.043','Material.045','Material.047','Material.046']},
+
+
+"P18.glb":{add:"./P18.glb",
+   "gemo":["mesh_21_1","mesh_21_2","mesh_21_4","mesh_21_0","mesh_21_3","mesh_21_5"],
+    "material": ['Material.070','Material.069','Material.066','Material.068','Material.071','Material.067']},
+
+
+
 
     
 
